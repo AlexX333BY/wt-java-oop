4aa4af9a66b169b598312c1e54851f306624bd52
Aliaksei <alexx333by@gmail.com>
2018-11-05 12:25:52 +0300

    Added static class containing command response messages

---
+++ src/by/bsuir/kaziukovich/oop/controller/CommandResponse.java
@@ -0,0 +1,21 @@
+package by.bsuir.kaziukovich.oop.controller;
+
+/**
+ * Static constructor containing constants for commands response
+ */
+public class CommandResponse {
+    /**
+     * Constant string to describe command execution success response
+     */
+    public final static String SUCCESS_RESPONSE = "SUCCESS";
+
+    /**
+     * Constant string to describe command execution failure response
+     */
+    public final static String FAILURE_RESPONSE = "FAILURE";
+
+    /**
+     * Private constructor to avoid object creation
+     */
+    private CommandResponse() { }
+}
