7113c5ed745da281499aeef41b076ada22c3ef74
Aliaksei <alexx333by@gmail.com>
2018-11-05 12:06:36 +0300

    Command enums added

---
+++ src/by/bsuir/kaziukovich/oop/controller/book/BookCommandName.java
@@ -0,0 +1,31 @@
+package by.bsuir.kaziukovich.oop.controller.book;
+
+/**
+ * Command names for books data
+ */
+public enum BookCommandName {
+    /**
+     * Command for loading data from file
+     */
+    LOAD_DATA,
+
+    /**
+     * Updates data in file
+     */
+    UPDATE_DATA,
+
+    /**
+     * Adds new book
+     */
+    ADD_BOOK,
+
+    /**
+     * Updates book with new info
+     */
+    UPDATE_BOOK,
+
+    /**
+     * Deletes book
+     */
+    DELETE_BOOK
+}
---
+++ src/by/bsuir/kaziukovich/oop/controller/user/UserCommandName.java
@@ -0,0 +1,36 @@
+package by.bsuir.kaziukovich.oop.controller.user;
+
+/**
+ * Command names for users data
+ */
+public enum UserCommandName {
+    /**
+     * Command for loading data from file
+     */
+    LOAD_DATA,
+
+    /**
+     * Updates data in file
+     */
+    UPDATE_DATA,
+
+    /**
+     * Adds new user
+     */
+    ADD_USER,
+
+    /**
+     * Updates user info
+     */
+    UPDATE_USER,
+
+    /**
+     * Deletes user
+     */
+    DELETE_USER,
+
+    /**
+     * Checks password
+     */
+    CHECK_PASSWORD
+}
