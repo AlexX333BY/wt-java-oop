ff95be726371d4feae3675f6232161d03e71c247
Aliaksei <alexx333by@gmail.com>
2018-11-06 14:26:04 +0300

    InfoChecker factory implemented

---
+++ src/by/bsuir/kaziukovich/oop/datalayer/checker/InfoCheckersFactory.java
@@ -0,0 +1,48 @@
+package by.bsuir.kaziukovich.oop.datalayer.checker;
+
+import by.bsuir.kaziukovich.oop.datalayer.checker.bookinfo.BookInfoChecker;
+import by.bsuir.kaziukovich.oop.datalayer.checker.bookinfo.impl.LibraryBookInfoChecker;
+import by.bsuir.kaziukovich.oop.datalayer.checker.userinfo.UserInfoChecker;
+import by.bsuir.kaziukovich.oop.datalayer.checker.userinfo.impl.LibraryUserInfoChecker;
+
+/**
+ * Checker factory class
+ */
+public class InfoCheckersFactory {
+    /**
+     * Single book info checker instance
+     */
+    private static BookInfoChecker bookInfoChecker = null;
+
+    /**
+     * Single user info checker instance
+     */
+    private static UserInfoChecker userInfoChecker = null;
+
+    /**
+     * Method for generating book info checker instance
+     * @return Book info checker instance
+     */
+    public static BookInfoChecker getBookInfoChecker() {
+        if (bookInfoChecker == null) {
+            bookInfoChecker = new LibraryBookInfoChecker();
+        }
+        return bookInfoChecker;
+    }
+
+    /**
+     * Method for generating user info checker instance
+     * @return User info checker instance
+     */
+    public static UserInfoChecker getUserInfoChecker() {
+        if (userInfoChecker == null) {
+            userInfoChecker = new LibraryUserInfoChecker();
+        }
+        return userInfoChecker;
+    }
+
+    /**
+     * Private constructor to avoid object creation
+     */
+    private InfoCheckersFactory() { }
+}
